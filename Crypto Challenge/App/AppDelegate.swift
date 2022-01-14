2d08d7d096aa0126f29254760ff012b85855e1e9
maughray <maughray@gmail.com>
2022-01-14 19:32:15 +0200

    Basic project structure

---
+++ Crypto Challenge/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Crypto Challenge
+//
+//  Created by Victor Morei on 14.01.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+   lazy var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
